be504bf36bb62f98bd60ca9d1da96740a47f792a
kumar-kalyan <67071462+kumar-kalyan@users.noreply.github.com>
2023-06-30 18:00:31 +0530

    Components made

---
+++ src/Body.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Body(props) {
+  return (
+    <>
+      <h1>Body Component</h1>
+    </>
+  );
+}
+export default Body;
---
+++ src/Card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Card(props) {
+  return (
+    <>
+      <h1>Card Component</h1>
+    </>
+  );
+}
+export default Card;
